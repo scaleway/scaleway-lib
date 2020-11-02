0b71730875f0d98d06915111df18db06640a573f
Emmanuel Chambon <echambon@scaleway.com>
2020-11-02 10:51:11 +0100

    feat: port shire countries codes into a dedicated package

---
+++ packages/countries/generate.py
@@ -0,0 +1,40 @@
+# This file will update subcountries directory.
+# For all new release of iso 6311
+# Requirements:
+#   - python
+#   - poetry
+#
+# Usage:
+#   $ poetry install && poetry run python generate.py
+# Or simply from the package.json
+#   $ yarn run generate
+
+import pycountry
+import json
+
+
+def main():
+    with open("./countries.json") as f:
+        data = json.load(f)
+
+    for countries in data:
+        sub_countries = pycountry.subdivisions.get(country_code=countries["code"])
+        if sub_countries:
+            with open("./subdivisions/" + countries["code"] + ".json", "w") as outfile:
+                arr = []
+                for b in sub_countries:
+                    arr.append(
+                        {
+                            "code": b.code,
+                            "country_code": b.country_code,
+                            "name": b.name,
+                            "parent_code": b.parent_code,
+                            "type": b.type,
+                        }
+                    )
+                arr.sort(key=lambda x: x["code"])
+                json.dump(arr, outfile, ensure_ascii=True, indent=2, sort_keys=True)
+
+
+if __name__ == "__main__":
+    main()
