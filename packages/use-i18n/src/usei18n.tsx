eee2e1df52ad5d462ce638381e6d5ebf10161633
Manu Chambon <manu@ehma.nu>
2021-09-01 17:25:21 +0200

    chore: drop intl-pluralrules polyfill (#357)

--- packages/use-i18n/src/usei18n.tsx
+++ packages/use-i18n/src/usei18n.tsx
@@ -11,7 +11,6 @@ import React, {
   useState,
 } from 'react'
 import ReactDOM from 'react-dom'
-import 'intl-pluralrules'
 import dateFormat, { FormatDateOptions } from './formatDate'
 import unitFormat, { FormatUnitOptions } from './formatUnit'
 import formatters, { IntlListFormatOptions } from './formatters'
