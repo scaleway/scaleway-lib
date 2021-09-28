cfa6858476a1fb7bfadd72577adc5f989d508d13
Manu Chambon <manu@ehma.nu>
2021-09-28 16:33:52 +0200

    feat(countries): add typescript definitions (#406)

---
+++ packages/countries/index.d.ts
@@ -0,0 +1,28 @@
+export interface Country {
+  name: string
+  dial_code: string
+  code: string
+  flag: string
+}
+
+export type Countries = Country[]
+
+export interface SubDivision {
+  code: string
+  country_code: string
+  name: string
+  parent_code: string | null
+  type: string
+}
+
+export type SubDivisions = SubDivision[]
+
+declare const List: Countries
+export default List
+
+declare module './subdivisions/*.json' {
+  const SubDivisionList: SubDivisions
+  export default SubDivisionList
+}
+
+export type AvailableCountries = import('./countries').AvailableCountries
