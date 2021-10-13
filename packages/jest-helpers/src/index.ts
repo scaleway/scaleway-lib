15108c00a58817ba82bb4f38c73dd281d1738f3a
Tom Lienard <tlienard@scaleway.com>
2021-10-13 15:17:16 +0200

    refactor(jest-helpers): remove initializeIfNeeded

--- packages/jest-helpers/src/index.ts
+++ packages/jest-helpers/src/index.ts
@@ -13,24 +13,13 @@ type Helpers<Theme> = {
 }
 
 export default function makeHelpers<Theme>(Wrapper: FC<{ theme?: Theme }>, createSerializerOptions?: CreateSerializerOptions): Helpers<Theme> {
-  let isInitialized = false
-
-  const initializeIfNeeded = <T>(callback: T) => {
-    if (!isInitialized) {
-      isInitialized = true
-
-      // use only class hash (generated from css style content)
-      expect.addSnapshotSerializer(
-        createSerializer(createSerializerOptions),
-      )
-    }
-
-    return callback;
-  }
+  expect.addSnapshotSerializer(
+    createSerializer(createSerializerOptions),
+  )
 
   const renderWithTheme = makeRenderWithTheme(Wrapper)
-  const shouldMatchEmotionSnapshot = initializeIfNeeded(makeShouldMatchEmotionSnapshot(renderWithTheme))
-  const shouldMatchEmotionSnapshotWithPortal = initializeIfNeeded(makeShouldMatchEmotionSnapshotWithPortal(renderWithTheme))
+  const shouldMatchEmotionSnapshot = makeShouldMatchEmotionSnapshot(renderWithTheme)
+  const shouldMatchEmotionSnapshotWithPortal = makeShouldMatchEmotionSnapshotWithPortal(renderWithTheme)
 
   return {
     renderWithTheme,
