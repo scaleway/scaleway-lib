fdeb304708e6cd64a38cd93805423b7a4ce2449b
Manu Chambon <manu@ehma.nu>
2022-01-12 14:25:28 +0100

    chore(use-i18n): move example tests file to json (#592)

--- packages/use-i18n/src/__tests__/locales/es.js
+++
@@ -1,7 +0,0 @@
-export default {
-  plurals:
-    'You have {numPhotos, plural, =0 {no photos.} =1 {one photo.} other {# photos.}}',
-  subtitle: 'Aquí hay un subtítulo',
-  'tests.test.namespaces': 'test',
-  title: 'Bienvenido @scaelway/ui i18n hook',
-}
--- packages/use-i18n/src/__tests__/locales/fr.js
+++
@@ -1,7 +0,0 @@
-export default {
-  plurals:
-    'You have {numPhotos, plural, =0 {no photos.} =1 {one photo.} other {# photos.}}',
-  subtitle: 'Voici un sous-titre',
-  'tests.test.namespaces': 'test',
-  title: 'Bienvenue sur @scaelway/ui i18n hook',
-}
