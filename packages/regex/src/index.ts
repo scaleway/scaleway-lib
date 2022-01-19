418bda9db7c3c342565e01433461d78079bb1b76
Adrien Gibrat <adrien.gibrat@gmail.com>
2022-01-19 16:40:05 +0100

    feat: add alphaLower and basicDomain regex (#600)
    Co-authored-by: Adrien Gibrat <agibrat@frontguys.fr>

--- packages/regex/src/index.ts
+++ packages/regex/src/index.ts
@@ -1,4 +1,5 @@
 export const alpha = /^[a-zA-Z]*$/
+export const alphaLower =  /^[a-z]+$/
 export const alphanum = /^[a-zA-Z0-9]*$/
 export const alphanumdash = /^[a-zA-Z0-9-]*$/
 export const alphanumdashdots = /^[a-zA-Z0-9-.]*$/
@@ -16,6 +17,7 @@ export const absoluteLinuxPath = /(^\/$|^(\/[a-zA-Z0-9_]+)*$)/
 // eslint-disable-next-line no-control-regex
 export const ascii = /^[\x00-\x7F]+$/
 export const backupKey = /^[A-Z0-9]{32}$/
+export const basicDomain = /^[a-z0-9-]+(\.[a-z0-9-]{1,63})+$/
 export const cron = /^[0-9,/*-]+$/
 export const digits = /^[0-9]*$/
 export const macAddress =
