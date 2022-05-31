18f16de88873969427a4fab92f748fca1df06cfa
Fabien Hébert <fhebert@scaleway.com>
2022-06-01 00:15:57 +0200

    fix: 🐛 another way to export

--- packages/use-dataloader/src/index.ts
+++ packages/use-dataloader/src/index.ts
@@ -1,7 +1,9 @@
+import { UseDataLoaderConfig } from './types'
+
 export {
   default as DataLoaderProvider,
   useDataLoaderContext,
 } from './DataLoaderProvider'
 export { default as useDataLoader } from './useDataLoader'
 export { default as usePaginatedDataLoader } from './usePaginatedDataLoader'
-export { UseDataLoaderConfig } from './types'
+export { UseDataLoaderConfig }
