aec2cc461fd0a11c460c8d22dd2eac776a63ea5d
QuiiBz <tom.lienrd@gmail.com>
2022-08-25 14:34:31 +0200

    refactor: remove prefix

--- packages/use-i18n/src/types.ts
+++ packages/use-i18n/src/types.ts
@@ -12,16 +12,14 @@ export type TranslateFn<Locale extends BaseLocale> = <
   Key extends LocaleKeys<Locale, undefined>,
   Value extends LocaleValue = ScopedValue<Locale, undefined, Key>,
 >(
-  key: Locale extends { prefix: string } ? `${Locale['prefix']}.${Key}` : Key,
+  key: Key,
   ...params: Params<Value>['length'] extends 0 ? [] : [ParamsObject<Value>]
 ) => string
 
 export type ScopedTranslateFn<Locale extends BaseLocale> = <
   Scope extends Scopes<Locale>,
 >(
-  scope: Locale extends { prefix: string }
-    ? `${Locale['prefix']}.${Scope}`
-    : Scope,
+  scope: Scope,
 ) => <
   Key extends LocaleKeys<Locale, Scope>,
   Value extends LocaleValue = ScopedValue<Locale, Scope, Key>,
