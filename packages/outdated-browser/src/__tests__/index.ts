538c13be8ac158f80058d919c10874f9a3c2a6d3
Manu Chambon <echambon@scaleway.com>
2022-10-14 19:56:11 +0200

    feat(outdated-browser): revamped banner design + save preference (#1036)
    * feat(outdated-browser): revamped banner design + save preference
    
    * fix: update tests
    
    * fix: update tests

--- packages/outdated-browser/src/__tests__/index.ts
+++ packages/outdated-browser/src/__tests__/index.ts
@@ -2,6 +2,25 @@
  * @jest-environment jsdom
  */
 
+const localStorageMock = (() => {
+  let store: Record<string, unknown> = {}
+
+  return {
+    getItem(key: string): unknown {
+      return store?.[key] || null
+    },
+    setItem(key: string, value: unknown) {
+      store[key] = value?.toString()
+    },
+    removeItem(key: string) {
+      delete store[key]
+    },
+    clear() {
+      store = {}
+    },
+  }
+})()
+
 describe('@outdated-browser', () => {
   const originUA = global.navigator.userAgent
   let fakeUA: string | undefined
@@ -12,11 +31,18 @@ describe('@outdated-browser', () => {
         return fakeUA ?? originUA
       },
     })
+
+    Object.defineProperty(window, 'sessionStorage', {
+      value: localStorageMock,
+    })
   })
 
   afterEach(() => {
     jest.resetModules()
     jest.restoreAllMocks()
+    sessionStorage.clear()
+    // Clear dom for next test
+    document.getElementsByTagName('html')[0].innerHTML = ''
     fakeUA = undefined
   })
 
@@ -39,10 +65,34 @@ describe('@outdated-browser', () => {
     })
   })
 
-  test('render banner if SUPPORTED_BROWSER is defined and does not mtch userAgen ', () => {
+  test('render banner if SUPPORTED_BROWSER is defined and does not match userAgent', () => {
     fakeUA = 'b'
     // @ts-expect-error global mock
     global.SUPPORTED_BROWSERS = 'a'
+
+    return import('..').then(() => {
+      expect(document.documentElement.innerHTML).toMatchSnapshot()
+    })
+  })
+
+  test('render nothing if SUPPORTED_BROWSER is defined and does not match userAgent and user has already ignored banner', () => {
+    fakeUA = 'b'
+    // @ts-expect-error global mock
+    global.SUPPORTED_BROWSERS = 'a'
+    sessionStorage.setItem('__outdated', 'true')
+
+    return import('..').then(() => {
+      expect(document.documentElement.innerHTML).toMatchSnapshot()
+    })
+  })
+
+  test('render nothing if SUPPORTED_BROWSER is defined and does not match userAgent and user click on button', () => {
+    fakeUA = 'b'
+    // @ts-expect-error global mock
+    global.SUPPORTED_BROWSERS = 'a'
+    // @ts-expect-error injected globally by module
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    closeOutdated()
 
     return import('..').then(() => {
       expect(document.documentElement.innerHTML).toMatchSnapshot()
