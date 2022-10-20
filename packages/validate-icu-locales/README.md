e45db3168601bb52c8dab030658e45bc7317905f
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2022-10-20 22:31:56 +0000

    feat(icu): use node instead of node-ts

--- packages/validate-icu-locales/README.md
+++ packages/validate-icu-locales/README.md
@@ -1,20 +1,46 @@
 # `@scaleway/validate-icu-locales`
 
-## A tiny hooks to handle segment events
-
-use [@segment/analytics-next](https://github.com/segmentio/analytics-next)
-
+## A tiny cli to handle ICU errors on locales files.
 ## Install
 
 ```bash
-$ pnpm add @scaleway/validate-icu-locales
+$ pnpm add -D @scaleway/validate-icu-locales
 ```
-
 ## Usage
 
-### Event directory
+We can parse JSON, TS and JS if theses files use default export.
 
-Create an events directory with all you specific events.
 
-Each event will have a format like this:
+```
+node @scaleway/validate-icu-locales "../**/en.json"
+```
 
+If there is an error on a local, the CLI will throw an Error and print all errors.
+
+
+## Error
+````
+export default from:  ../src/__tests__/locales/en-1.js  is not an object
+{
+  errors: [
+    {
+      err: [SyntaxError],
+      value: '{count, plural, =0 {Minute} =1 {Minute} other {Minutes',
+      key: 'units.minutes.label',
+      filePath: '../src/__tests__/locales/en.js'
+    },
+    {
+      err: [SyntaxError],
+      value: '{count, plural, =0 {Minute} =1 {Minute} other {Minutes',
+      key: 'units.minutes.label',
+      filePath: '../src/__tests__/locales/en.json'
+    },
+    {
+      err: [SyntaxError],
+      value: '{count, plural, =0 {Minute} =1 {Minute} other {Minutes',
+      key: 'units.minutes.label',
+      filePath: '../src/__tests__/locales/en.ts'
+    }
+  ]
+}
+```
--- packages/validate-icu-locales/src/index.ts
+++ packages/validate-icu-locales/src/index.ts
@@ -1,52 +1,82 @@
 import { parse } from '@formatjs/icu-messageformat-parser'
-import glob from 'glob'
+import { info } from 'console'
+import fs from 'fs'
+import { globby } from 'globby'
+import { importFromStringSync } from 'module-from-string'
 
 const args = process.argv.slice(2)
-const paths = args[0]
+const pattern = args[0]
 
-const { error, table, info } = console
+const { error, table } = console
 
-info(paths)
+type Err = {
+  err: unknown
+  value: string
+  key: string
+  filePath: string
+}
+const errors: Err[] = []
 
-const findICUError = (
-  locales: { [key: string]: string },
-  filePath?: string,
-) => {
+const findICUError = (locales: { [key: string]: string }, filePath: string) => {
   Object.keys(locales).forEach(key => {
     const value = locales[key]
 
     try {
       parse(value)
     } catch (err) {
-      error({ err, value, key, filePath })
+      errors.push({
+        err,
+        value,
+        key,
+        filePath,
+      })
     }
   })
 }
 
 const readFiles = (files: string[]) =>
   files.forEach(file => {
-    import(file)
-      .then(({ default: locales }: { default: Record<string, string> }) => {
+    const extension = file.split('.').pop()
+
+    if (extension === 'json') {
+      const json = fs.readFileSync(file).toString()
+      try {
+        const locales = JSON.parse(json) as Record<string, string>
+
         findICUError(locales, file)
-      })
-      .catch(err => {
+      } catch (err) {
         error(err)
-      })
-  })
-
-const main = () => {
-  glob(paths, (err, files) => {
-    if (err) {
-      error({
-        err,
-      })
+      }
     }
 
-    if (files) {
-      table(files)
-      readFiles(files)
+    if (extension === 'ts' || extension === 'js') {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const d: { default: Record<string, string> } = importFromStringSync(
+        fs.readFileSync(file).toString(),
+        { transformOptions: { loader: 'ts' } },
+      )
+
+      if (typeof d.default === 'object') {
+        findICUError(d.default, file)
+      } else {
+        error('export default from: ', file, ' is not an object')
+      }
     }
   })
+
+const files = await globby(pattern)
+
+if (files.length > 0) {
+  table(files)
+  readFiles(files)
 }
 
-main()
+if (files.length === 0) {
+  info('There is no files matching this pattern', pattern)
+}
+
+if (errors.length > 1) {
+  error({ errors })
+  info(new Error('There is somes ICU error'))
+  process.exit(1)
+}
