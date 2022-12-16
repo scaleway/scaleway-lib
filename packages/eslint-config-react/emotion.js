63d973640d4b4706886b78dae0068fb8f55c8dd5
Emmanuel Chambon <echambon@scaleway.com>
2022-12-16 17:38:03 +0100

    fix: correct css prop

--- packages/eslint-config-react/emotion.js
+++ packages/eslint-config-react/emotion.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: ['@emotion'],
   rules: {
+    'react/no-unknown-property': [
+      'error',
+      {
+        ignore: ['css'],
+      },
+    ],
     '@emotion/import-from-emotion': 'error',
     '@emotion/no-vanilla': 'error',
     '@emotion/styled-import': 'error',
