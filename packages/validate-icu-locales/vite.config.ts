346afafb4a71e31f8b77df20fc2c6bed3aa6a483
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2024-04-23 00:18:30 +0000

    chore(vite): migrate build from rollup to vite

---
+++ packages/validate-icu-locales/vite.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable import/no-relative-packages */
+import { defineConfig, mergeConfig } from 'vite'
+import { defaultConfig } from '../../vite.config'
+
+export default mergeConfig(defineConfig(defaultConfig), {
+  build: {
+    target: ['node20'],
+  },
+})
