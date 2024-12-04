ac4d14c61be16e7d5c8d1eb9bcbccbcbd1b06852
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2024-12-04 10:22:36 +0000

    fix(eslint): confict rules
    Signed-off-by: Alexandre Philibeaux <aphilibeaux@scaleway.com>

--- packages/eslint-config-react/typescript.mjs
+++ packages/eslint-config-react/typescript.mjs
@@ -19,6 +19,7 @@ export default [
       'airbnb-base',
       'plugin:@typescript-eslint/recommended',
       'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:import/typescript',
     ),
   ),
   ...airbnbTypescript,
