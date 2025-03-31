795584a6bea1e9e8e398fbf7ffbbab32cb8d77f9
mjolly <mjolly@scaleway.com>
2025-03-31 16:40:37 +0200

    feat(regex): remove @ first leter for regex password

--- packages/regex/src/index.ts
+++ packages/regex/src/index.ts
@@ -94,4 +94,4 @@ export const sgPortRange =
   /^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5]?[0-9]{1,4})(-(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5]?[0-9]{1,4}))?$/
 
 // Include all characters except backtick `
-export const password = /^[^`]+$/
+export const password = /^(?!@)[^`]*$/
