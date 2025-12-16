e13691abb36c69662777a0738a2b75c445108055
mjolly <mjolly@scaleway.com>
2025-12-16 16:36:15 +0100

    feat: add webhosting username email regex

--- packages/regex/src/__tests__/index.test.ts
+++ packages/regex/src/__tests__/index.test.ts
@@ -54,6 +54,7 @@ import {
   uppercaseBasicSubdomain,
   url,
   uuid,
+  webhostingUsernameEmailRegex,
 } from '..'
 
 const alphanumDashDotsText = 'testwithdashdots-.'
@@ -1164,6 +1165,25 @@ describe('@regex', () => {
       ['user..name', false],
     ])('should match regex %s to be %s', (string, expected) => {
       expect(kafkaUsernameRegex.test(string)).toBe(expected)
+    })
+  })
+
+  describe('webhostingUsernameEmailRegex', () => {
+    test.each([
+      ['test', true],
+      ['test_test', true],
+      ['test-test', true],
+      ['test.test', true],
+      ['test.test.', false],
+      ['test..test', false],
+      ['test-test-', false],
+      ['test_test_', false],
+      ['test.test.', false],
+      ['test..test', false],
+      ['test..test', false],
+      ['test..test', false],
+    ])('should match regex %s to be %s', (string, expected) => {
+      expect(webhostingUsernameEmailRegex.test(string)).toBe(expected)
     })
   })
 
