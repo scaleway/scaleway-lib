12e8daad1bef649b9136e437c83f59f31c0e75cb
Emmanuel CHAMBON <echambon@scaleway.com>
2026-05-15 10:17:37 +0200

    fix: feedbacks
    Signed-off-by: Emmanuel CHAMBON <echambon@scaleway.com>

--- packages/use-analytics/tsdown.config.ts
+++ packages/use-analytics/tsdown.config.ts
@@ -4,6 +4,7 @@ import { defaultConfig } from '../../tsdown.config.ts'
 export default mergeConfig(
   defaultConfig,
   defineConfig({
+    exports: false, // Disabled because it does not handle properly types exports with multiple entries as of yet
     entry: ['src/index.ts', 'src/analytics/index.ts', 'src/cookies-consent/index.ts'],
   }),
 )
