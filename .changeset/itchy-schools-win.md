f1492757a3025afc8214a4719784b2c70cafcc8b
Emmanuel CHAMBON <echambon@scaleway.com>
2026-07-20 10:18:31 +0200

    feat(units): add HOURS_IN_YEAR constant
    Signed-off-by: Emmanuel CHAMBON <echambon@scaleway.com>

---
+++ .changeset/itchy-schools-win.md
@@ -0,0 +1,5 @@
+---
+"@scaleway/units": patch
+---
+
+add HOURS_IN_YEAR constant
--- packages/units/src/constants.ts
+++ packages/units/src/constants.ts
@@ -11,5 +11,6 @@ export const DAYS_IN_YEAR = convertDuration(1, { from: 'years', to: 'days' })
 
 export const HOURS_IN_DAY = convertDuration(1, { from: 'days', to: 'hours' })
 export const HOURS_IN_MONTH = convertDuration(1, { from: 'months', to: 'hours' })
+export const HOURS_IN_YEAR = convertDuration(1, { from: 'years', to: 'hours' })
 
 export const MONTHS_IN_YEAR = convertDuration(1, { from: 'years', to: 'months' })
